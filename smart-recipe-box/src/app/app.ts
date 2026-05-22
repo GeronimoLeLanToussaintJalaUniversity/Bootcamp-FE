edfaa75348f93bfc0c6f8d4a28456a9867feed55
Gero Le Lan <Geronimo.LeLan@jala.university>
2026-05-22 15:53:12 -0300

    feat: complete module 6

--- smart-recipe-box/src/app/app.ts
+++ smart-recipe-box/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal ,computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RecipeModel } from './models';
 import { MOCK_RECIPES } from './mock-recipes';
@@ -12,13 +12,21 @@ import { MOCK_RECIPES } from './mock-recipes';
 export class App {
   protected readonly recipe = signal(MOCK_RECIPES[0] as RecipeModel);
   protected readonly title = signal('My Recipe Box');
+    protected readonly servings = signal(1);
+  protected readonly adjustedIngredients = computed(() => {
+    const servings = this.servings();
+    return this.recipe().ingredients.map(ingredient => ({
+      ...ingredient,
+      quantity: ingredient.quantity * servings
+    })); 
+  });
+
   protected showSpaghetti(): void {
     this.recipe.set(MOCK_RECIPES[0] as RecipeModel);
   }
   protected showSalad(): void {
     this.recipe.set(MOCK_RECIPES[1] as RecipeModel);
   }
-  protected readonly servings = signal(1);
   protected increment(): void {
     this.servings.update(current => current + 1);
   }
