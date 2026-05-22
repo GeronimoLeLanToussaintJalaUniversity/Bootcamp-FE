1236458c35d436849754fefb63ff258db051c7e1
Gero Le Lan <Geronimo.LeLan@jala.university>
2026-05-22 14:42:49 -0300

    feat: complete module 2

--- smart-recipe-box/src/app/app.ts
+++ smart-recipe-box/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('smart-recipe-box');
+  protected readonly title = signal('My Recipe Box');
 }
