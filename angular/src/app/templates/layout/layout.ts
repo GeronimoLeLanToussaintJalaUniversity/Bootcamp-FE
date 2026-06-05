5fe826f7f99bb61987005647d065f2e85a362d1d
Gero Le Lan <Geronimo.LeLan@jala.university>
2026-06-05 14:57:43 -0300

    test: using UpperCasePipe, LowerCasePipe and TitleCasePipe

--- angular/src/app/templates/layout/layout.ts
+++ angular/src/app/templates/layout/layout.ts
@@ -1,13 +1,19 @@
 import { Component, signal, computed, HostListener, inject } from '@angular/core';
+import { HasRoleDirective } from '../../directives/has-role.directive';
+import { AuthService } from '../../services/auth.service';
+import { UpperCasePipe, LowerCasePipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
+  imports: [HasRoleDirective, UpperCasePipe, LowerCasePipe, TitleCasePipe],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
 export class Layout {
+  authService = inject(AuthService);
   windowWidth = signal(window.innerWidth);
   manualOverride = signal<boolean | null>(null);
+  roles = "hola como vas";
 
   sidebarVisible = computed(() => {
     if (this.manualOverride() !== null) return this.manualOverride();
@@ -17,7 +23,7 @@ export class Layout {
   @HostListener('window:resize')
   onResize() {
     this.windowWidth.set(window.innerWidth);
-    if (this.manualOverride() ) {
+    if (this.manualOverride()) {
       this.manualOverride.set(null);
     }
   }
