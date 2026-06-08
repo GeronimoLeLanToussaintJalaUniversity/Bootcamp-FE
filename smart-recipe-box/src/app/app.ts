a1884c8310e1431f04abe94556665097c0492849
Gero Le Lan <Geronimo.LeLan@jala.university>
2026-06-08 12:52:23 -0300

    feat: complete module 13

--- smart-recipe-box/src/app/app.ts
+++ smart-recipe-box/src/app/app.ts
@@ -1,27 +1,22 @@
-import { Component, signal   } from '@angular/core';
-import { RecipeModel } from './models';
+import { Component, computed, signal } from '@angular/core';
 import { MOCK_RECIPES } from './mock-recipes';
 import { RecipeCard } from './recipe-card/recipe-card';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RecipeCard],
+  imports: [RecipeCard, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('My Recipe Box');
-  protected readonly recipe = signal(MOCK_RECIPES[0] as RecipeModel);
-
-
-  protected showSpaghetti(): void {
-    this.recipe.set(MOCK_RECIPES[0] as RecipeModel);
-  }
-  protected showSalad(): void {
-    this.recipe.set(MOCK_RECIPES[1] as RecipeModel);
-  }
-
-
+  protected readonly recipes = signal(MOCK_RECIPES);
+  protected readonly searchTerm = signal('');
+  protected readonly filteredRecipes = computed(() => {
+    const term = this.searchTerm().toLowerCase();
+    return this.recipes().filter(recipe => recipe.name.toLowerCase().includes(term));
+  });
 }
 
 
